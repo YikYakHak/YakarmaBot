5f3ded29318838b67bd495f8a1cdb9991b46d156
Canzhi Ye <canzhiye@gmail.com>
2014-09-27 03:23:29 -0700

    kill_feed feature

--- yikyak.py
+++ yikyak.py
@@ -6,7 +6,7 @@ with open('upvote.txt', 'r') as f:
 with open('downvote.txt', 'r') as f:
 	downvotes = f.read().split()
 
-messageID = "R/5426622e13f82822d9c4e69b9bb27"
+messageID = "R/54267bf93c77b60e5f66beca3bdde"
 
 Ann_Arbor = ['42.2814', "-83.7483"]
 Austin = ['30.2861','-97.7394']
@@ -14,30 +14,50 @@ Berkeley = ['37.8717', '-122.2728']
 Columbus = ['39.9833', '-82.9833']
 Philadelphia = ['39.9500', '-75.1900']
 State_College = ['40.7914', '-77.8586']
-
-
+Columbus = ['40.0000', '-83.0145']
 
 city = Berkeley
 lat = city[0]
 lng = city[1]
 
-def like_yak():
-	for line in upvotes:
-		yikyak_header =  { "Host": "yikyakapp.com", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5", "Connection": "keep-alive", "User-Agent": "Yik Yak/2.0.2.5 (iPhone; iOS 7.0.6; Scale/2.00)" }
-		register = requests.post('https://yikyakapp.com/api/registerUser?userID=' + line.upper() +'&salt=123&hash=abc', headers = yikyak_header)
-		send_like = requests.get("https://yikyakapp.com/api/likeMessage?messageID=" + messageID + "&userID=" + line.upper() + "&salt=1411725084&hash=jxuHrrXzeK1LwTC5JlsQWsUOuzo%3D&userLat=37.873325&userLong=-122.252983")
-		print('sent like')
+def kill_feed(feed):
+	i = 0
+	while i < len(feed): 
+		n = 0
+		while n <= int(feed[i]['numberOfLikes']) + 4:
+			dislike_yak(feed[i]['messageID'], downvotes[n])
+
+			if int(feed[i]['numberOfLikes']) - n < - 4:
+				n = 999
+				print('killed a post')
+			else:
+				n += 1
+		i += 1
+
+def mass_like_yak(m_id):
+	for user_id in upvotes:
+		like_yak(m_id, user_id)
 	f.close()
 
-def dislike_yak():
-	for line in downvotes: 
-		yikyak_header =  { "Host": "yikyakapp.com", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5", "Connection": "keep-alive", "User-Agent": "Yik Yak/2.0.2.5 (iPhone; iOS 7.0.6; Scale/2.00)" }
-		register = requests.post('https://yikyakapp.com/api/registerUser?userID=' + line.upper() +'&salt=123&hash=abc', headers = yikyak_header)
-		send_downvote = requests.get("https://yikyakapp.com/api/downvoteMessage?messageID=" + messageID + "&userID=" + line.upper() + "&salt=1411725084&hash=jxuHrrXzeK1LwTC5JlsQWsUOuzo%3D&userLat=37.873325&userLong=-122.252983")
-		print('sent downvotes')
+def like_yak(m_id, user_id):
+	yikyak_header =  { "Host": "yikyakapp.com", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5", "Connection": "keep-alive", "User-Agent": "Yik Yak/2.0.2.5 (iPhone; iOS 7.0.6; Scale/2.00)" }
+	register = requests.post('https://yikyakapp.com/api/registerUser?userID=' + user_id +'&salt=123&hash=abc', headers = yikyak_header)
+	send_like = requests.get("https://yikyakapp.com/api/likeMessage?messageID=" + m_id + "&userID=" + user_id + "&salt=1411725084&hash=jxuHrrXzeK1LwTC5JlsQWsUOuzo%3D&userLat=37.873325&userLong=-122.252983")
+	print('sent like')
+
+def mass_dislike_yak(m_id):
+	for user_id in downvotes: 
+		dislike_yak(m_id, user_id)
 	f.close()
 
+def dislike_yak(m_id, user_id):
+	yikyak_header =  { "Host": "yikyakapp.com", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5", "Connection": "keep-alive", "User-Agent": "Yik Yak/2.0.2.5 (iPhone; iOS 7.0.6; Scale/2.00)" }
+	register = requests.post('https://yikyakapp.com/api/registerUser?userID=' + user_id +'&salt=123&hash=abc', headers = yikyak_header)
+	send_downvote = requests.get("https://yikyakapp.com/api/downvoteMessage?messageID=" + m_id + "&userID=" + user_id + "&salt=1411725084&hash=jxuHrrXzeK1LwTC5JlsQWsUOuzo%3D&userLat=37.873325&userLong=-122.252983")
+	for message in get_yaks():
+		if m_id in message.values():
+			print('sent downvote ' + message['message'])
 
 def get_yaks():
 	yaks = requests.get("https://yikyakapp.com/api/getMessages?userID=f6bb54ed-d969-4260-b101-9b455e564016&lat="+lat+"&long="+lng+"&salt=1411724954&hash=vuhZnitPbE4p8agZ5sBUub2KUvQ%3D&userLat="+lat+"&userLong="+lng)
-	print(yaks.json()["messages"])
+	return yaks.json()["messages"]
